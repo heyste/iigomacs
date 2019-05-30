509f760d7d10d4a343a1ca928fad1027ca5cfc60
Stephen Heywood <stephen@heyste.nz>
2019-05-30 18:31:06 +1200

    Update volume mapping to /tmp/iigomacs:/home/emacs

--- boot-root.sh
+++ boot-root.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Booting iigomacs..."
-[ ! -d "/tmp/user" ] && mkdir /tmp/user
+[ ! -d "/tmp/iigomacs" ] && mkdir /tmp/iigomacs
 CURRENT_UID=0:0 docker-compose up -d --build
 docker exec -it iigomacs_iigomacs_1 /bin/bash -c "cd ~ && /bin/bash"
