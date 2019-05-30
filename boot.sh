ad7bab5c70a1166e2537df43e924b01c3a0a477c
Stephen Heywood <stephen@heyste.nz>
2019-05-30 13:57:55 +1200

    Rebuild container if required

--- boot.sh
+++ boot.sh
@@ -2,5 +2,5 @@
 
 echo "Booting iigomacs..."
 [ ! -d "/tmp/user" ] && mkdir /tmp/user
-CURRENT_UID=$(id -u):$(id -g) docker-compose up -d
+CURRENT_UID=$(id -u):$(id -g) docker-compose up -d --build
 docker exec -it iigomacs_iigomacs_1 /bin/bash -c "cd ~ && /bin/bash"
