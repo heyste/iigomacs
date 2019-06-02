9a196f6b8157a5adf02a0f5420f1a47a56a5e9d4
Stephen Heywood <stephen@heyste.nz>
2019-06-03 11:02:17 +1200

    Add scripts to join a running container

---
+++ join-as-root.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Join iigomacs..."
+if  docker ps | grep iigomacs > /dev/null ; then
+  echo "Connecting to iigomacs..."
+  CURRENT_ID=0:0
+  docker exec -it -u $CURRENT_ID iigomacs_iigomacs_1 /bin/bash -c "cd ~ && /bin/bash"
+else
+ echo "iigomacs not running. Start with './boot.sh'"
+fi
