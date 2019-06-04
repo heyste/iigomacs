ee33990e43db2d03f85b1ebd698e059d17878e44
Stephen Heywood <stephen@heyste.nz>
2019-06-04 21:27:21 +1200

    Replace env CURRENT_UID for user with CURRENT_ID

--- boot-as-root.sh
+++ boot-as-root.sh
@@ -2,5 +2,5 @@
 
 echo "Booting iigomacs..."
 [ ! -d "/tmp/iigomacs" ] && mkdir /tmp/iigomacs
-CURRENT_UID=0:0 docker-compose up -d --build
+CURRENT_ID=0:0 docker-compose up -d --build
 docker exec -it iigomacs_iigomacs_1 /bin/bash -c "cd ~ && /bin/bash"
--- boot.sh
+++ boot.sh
@@ -2,5 +2,5 @@
 
 echo "Booting iigomacs..."
 [ ! -d "/tmp/iigomacs" ] && mkdir /tmp/iigomacs
-CURRENT_UID=$(id -u):$(id -g) docker-compose up -d --build
+CURRENT_ID=$(id -u):$(id -g) docker-compose up -d --build
 docker exec -it iigomacs_iigomacs_1 /bin/bash -c "cd ~ && /bin/bash"
